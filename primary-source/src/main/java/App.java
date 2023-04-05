6f1102849d3d8a65a97b0917c9fe355db5dace76
David Vargas <davidhockeysp@hotmail.com>
2023-04-05 18:32:08 -0500

    Se agrega el proyecto

---
+++ primary-source/src/main/java/App.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class App {
+    public static void main(String[] args) {
+        // Set up the WebDriver
+        //System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver"); // Replace with the path to your webdriver if necessary
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(co);
+
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+
+        // Find the search box and enter the search term
+        WebElement searchBox = driver.findElement(By.name("search"));
+        searchBox.sendKeys("ChatGpt");
+        searchBox.sendKeys(Keys.RETURN);
+
+        // Wait for the page to load and print the title
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println(driver.getTitle());
+
+        // Quit the WebDriver
+        driver.quit();
+    }
+}
